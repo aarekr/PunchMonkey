67e4bcb97ed613c2ca9201e349289cbee0d8ea01
aarekr <aare212@hotmail.com>
2024-12-14 15:26:57 +0200

    Drawing ellipses on the screen

--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-""" User Interface """
+""" User Interface module """
 
 import sys
 import pygame
@@ -10,6 +10,7 @@ YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
 
 BOARD_SIZE = (950, 750)
+RADIUS = 50
 
 def game_start_text():
     """ Displaying game start text """
@@ -19,17 +20,28 @@ def game_start_text():
     return label
 
 class UI:
-    """ User interface for the game """
+    """ User interface class """
     def __init__(self):
         self.game_active = True
         self.screen = pygame.display.set_mode(BOARD_SIZE)
 
     def start_game(self):
         self.game_loop()
+    
+    def draw_ellipses(self):
+        y = 200
+        while y <= 700:
+            x = 100
+            while x <= 700:
+                pygame.draw.ellipse(self.screen, WHITE, (x, y, 100, 20))
+                x += 200
+            y += 150
+        pygame.display.update()
 
     def game_loop(self):
         pygame.init()
         self.screen.blit(game_start_text(), (250, 15))
+        #self.draw_ellipses()
         pygame.display.update()
 
         print("Game started")
@@ -37,4 +49,4 @@ class UI:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                pygame.display.update()
+                self.draw_ellipses()
