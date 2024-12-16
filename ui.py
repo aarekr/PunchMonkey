17f7cfdd2069442b1883b08a472acfefb939e36c
aarekr <aare212@hotmail.com>
2024-12-16 17:55:00 +0200

    Game start functionality

--- ui.py
+++ ui.py
@@ -15,11 +15,18 @@ BOARD_SIZE = (950, 750)
 RADIUS = 50
 DEFAULT_IMAGE_SIZE = (70, 70)
 
-def game_start_text():
-    """ Displaying game start text """
+def game_top_text():
+    """ Displaying Punch Monkey text """
     text_font = pygame.font.Font(pygame.font.get_default_font(), 60)
     game_starts = "Punch Monkey"
-    label = text_font.render(game_starts, 0, YELLOW)
+    label = text_font.render(game_starts, 0, RED)
+    return label
+
+def game_start_text():
+    """ Displaying game start text """
+    text_font = pygame.font.Font(pygame.font.get_default_font(), 40)
+    game_starts = "Press s to start the game"
+    label = text_font.render(game_starts, 0, BLUE)
     return label
 
 class UI:
@@ -30,6 +37,18 @@ class UI:
         self.interval = 2
 
     def start_game(self):
+        pygame.init()
+        self.screen.blit(game_top_text(), (250, 15))
+        self.screen.blit(game_start_text(), (210, 250))
+        pygame.display.update()
+        start_game = False
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_s:
+                        start_game = True
+            if start_game == True:
+                break
         self.game_loop()
     
     def draw_ellipses(self):
@@ -37,14 +56,14 @@ class UI:
         while y <= 700:
             x = 100
             while x <= 700:
-                pygame.draw.ellipse(self.screen, WHITE, (x, y, 100, 20))
+                pygame.draw.ellipse(self.screen, BLACK, (x, y, 100, 20))
                 x += 200
             y += 150
         pygame.display.update()
 
     def game_loop(self):
         pygame.init()
-        self.screen.blit(game_start_text(), (250, 15))
+        self.screen.blit(game_top_text(), (250, 15))
         monkey_image = pygame.image.load("assets/monkey_eyes_covered.jpg")
         monkey_image = pygame.transform.scale(monkey_image, DEFAULT_IMAGE_SIZE)
         pygame.display.update()
@@ -58,8 +77,8 @@ class UI:
             time.sleep(self.interval)
             self.interval -= 0.01
             print("changing monkey position, interval:", self.interval)
-            self.screen.fill(BLACK)
-            self.screen.blit(game_start_text(), (250, 15))
+            self.screen.fill(WHITE)
+            self.screen.blit(game_top_text(), (250, 15))
             self.draw_ellipses()
             x = random.choice([100, 300, 500, 700]) + 18
             y = random.choice([200, 350, 500, 650]) - 60
